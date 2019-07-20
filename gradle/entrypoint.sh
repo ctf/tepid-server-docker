68f77ddaa28bf51f196825a1442b259f03cdae07
lilatomic <daniel.goldman2@mail.mcgill.ca>
2019-07-19 20:32:38 -0400

    make manual building easier and better

--- gradle/entrypoint.sh
+++ gradle/entrypoint.sh
@@ -2,11 +2,15 @@
 echo "beginning build"
 cd /home/gradle/tepid-server
 gradle --status
-gradle test --continue || true
-gradle integrationTest || true
+gradle test -i --continue || true
 gradle war;
 echo "build completed"
+
 echo "deploying"
 rm -r /var/lib/tomcat8/webapps/tepid.war || true
-mv /home/gradle/tepid-server/build/libs/tepid.war /var/lib/tomcat8/webapps
+mv /home/gradle/tepid-server/build/libs/tepid*.war /var/lib/tomcat8/webapps/tepid.war
 echo "deploy complete"
+
+echo "running integration tests"
+gradle integrationTest -i || true
+echo "completed integration tests"
